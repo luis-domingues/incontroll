ca3c32a69a090cdd84a4980b5807ecb76d95891a
luis-domingues <luisdomingues117@gmail.com>
2025-06-13 08:47:40 -0300

    feat: implement MediatR validation pipeline behavior

--- InControll.Application/Validators/ValidationBehavior.cs
+++ InControll.Application/Validators/ValidationBehavior.cs
@@ -1,6 +1,27 @@
+using FluentValidation;
+using MediatR;
+
 namespace InControll.Application.Validators;
 
-public class ValidationBehavior
+public class ValidationBehavior<TRquest, TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
 {
+    private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+    public ValidationBehavior(IEnumerable<IValidator<TRequest>> validators)
+    {
+        _validators = validators;
+    }
     
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        if (_validators.Any())
+        {
+            var context = new ValidationContext<TRequest>(request);
+            var validationResults = await Task.WhenAll(_validators.Select(v => v.ValidateAsync(context, cancellationToken)));
+            var failures = validationResults.SelectMany(r => r.Errors).Where(f => f != null).ToList();
+            
+            if(failures.Any()) throw new ValidationException(failures);
+        }
+        return await next();
+    }
 }
